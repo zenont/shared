50715ed89e354b89a52521d4a7470f82ea403195
Zenon Trujillo <zenon.t@outlook.com>
2019-07-03 23:29:20 -0700

    chore: test

---
+++ packages/core/src/util/env/manager.ts
@@ -0,0 +1,56 @@
+import { NodeEnv, KnownEnvKey } from './types'
+import {
+  isNodeEnvironment,
+  isDebug,
+  hasEnvVar,
+  getEnvVar,
+  setEnvVar
+} from './util'
+
+export class NodeProcessEnvMgr<
+  T extends NodeEnv = NodeEnv,
+  K extends KnownEnvKey = KnownEnvKey
+> {
+  has(key: K) {
+    return hasEnvVar(key)
+  }
+
+  get(key: K): string | undefined {
+    return getEnvVar(key)
+  }
+
+  set(key: K, value: string): void {
+    setEnvVar(key, value)
+  }
+
+  isDebug() {
+    return isDebug()
+  }
+
+  isEnv(envName: T) {
+    return isNodeEnvironment(envName)
+  }
+
+  isDevelopment() {
+    return isNodeEnvironment('development')
+  }
+
+  isTest() {
+    return isNodeEnvironment('test')
+  }
+
+  isStaging() {
+    return isNodeEnvironment('staging')
+  }
+
+  isProduction() {
+    return isNodeEnvironment('production')
+  }
+}
+
+export function provideEnv<
+  T extends NodeEnv = NodeEnv,
+  K extends KnownEnvKey = KnownEnvKey
+>() {
+  return new NodeProcessEnvMgr<T, K>()
+}
