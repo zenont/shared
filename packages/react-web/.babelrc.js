895a4474d346a586b18c580cda6318083a0ec9c4
Zenon Trujillo <zenon.t@outlook.com>
2019-07-19 00:58:57 -0700

    chore: using rollup

--- packages/react-web/.babelrc.js
+++ packages/react-web/.babelrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-typescript',
+    '@babel/preset-react'
+  ],
   plugins: [
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread'
