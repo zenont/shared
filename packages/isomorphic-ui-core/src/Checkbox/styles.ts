417f56f0d703142a42052952381fc6494a9d5f4e
Zenon Trujillo <zenon.t@outlook.com>
2019-07-23 00:26:51 -0700

    chore: adding storybook

---
+++ packages/isomorphic-ui-core/src/Checkbox/styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+export const Layout = styled.div`
+  display: inline-block;
+  cursor: pointer;
+`
+
+export const Input = styled.input`
+  cursor: pointer;
+`
+
+export const Label = styled.label`
+  cursor: pointer;
+`
