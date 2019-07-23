50fc69b1ba2bac5db11dfd931783baee25776b02
Zenon Trujillo <zenon.t@outlook.com>
2019-07-22 22:42:17 -0700

    feat: removing encoding option

--- packages/isomorphic-util/src/encoding/base64.ts
+++ packages/isomorphic-util/src/encoding/base64.ts
@@ -2,31 +2,19 @@ import { base64EncArr } from './browser-util'
 import { strToUTF8Arr, base64DecToArr, UTF8ArrToStr } from './browser-utf8'
 import { isBrowser, isWebWorker } from '../env'
 
-export type Encoding =
-  | 'ascii'
-  | 'utf8'
-  | 'utf-8'
-  | 'utf16le'
-  | 'ucs2'
-  | 'ucs-2'
-  | 'base64'
-  | 'latin1'
-  | 'binary'
-  | 'hex'
-
-export const fromBase64 = (value: string, encoding?: Encoding) => {
+export const fromBase64 = (value: string) => {
   if (isBrowser() || isWebWorker()) {
     const aMyUTF8Output = base64DecToArr(value)
     return UTF8ArrToStr(aMyUTF8Output)
   }
 
-  return Buffer.from(`${value}`, 'base64').toString(encoding)
+  return Buffer.from(`${value}`, 'base64').toString('utf8')
 }
 
-export const toBase64 = (value: string, encoding?: Encoding) => {
+export const toBase64 = (value: string) => {
   if (isBrowser() || isWebWorker()) {
     const aMyUTF8Input = strToUTF8Arr(value)
     return base64EncArr(aMyUTF8Input)
   }
-  return Buffer.from(value, encoding).toString('base64')
+  return Buffer.from(value, 'utf8').toString('base64')
 }
