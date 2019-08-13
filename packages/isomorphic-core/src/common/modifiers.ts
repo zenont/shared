03a3e722f8c9ffce5271e09dba48790ed75ee867
Zenon Trujillo <zenon.t@outlook.com>
2019-08-13 00:56:11 -0700

    feat: adding modifiers

--- packages/isomorphic-core/src/common/modifiers.ts
+++ packages/isomorphic-core/src/common/modifiers.ts
@@ -1,2 +1,18 @@
 export type Maybe<T> = T | null | undefined
 export type Null<T> = T | null
+export type MutableRequired<T> = { -readonly [P in keyof T]-?: T[P] }
+export type ReadonlyPartial<T> = { +readonly [P in keyof T]+?: T[P] }
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+    ? DeepPartial<U>[]
+    : T[P] extends readonly (infer U)[]
+    ? readonly DeepPartial<U>[]
+    : DeepPartial<T[P]>
+}
+export type DeepRequired<T> = {
+  [P in keyof T]-?: T[P] extends (infer U)[]
+    ? DeepRequired<U>[]
+    : T[P] extends readonly (infer U)[]
+    ? readonly DeepRequired<U>[]
+    : DeepRequired<T[P]>
+}
